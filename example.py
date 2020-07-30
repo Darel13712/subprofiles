70b0ebf4c52dc6570c763b172cb284c32a29ce07
Darel13712 <darel142857@gmail.com>
2020-07-31 01:44:37 +0300

    reranking function

--- example.py
+++ example.py
@@ -1,10 +1,10 @@
 from rs_datasets import MovieLens
 
-from subprofiles import subprofiles
+from subprofiles import get_subprofiles
 from utils import to_csc, encode, get_knn
 
 df = MovieLens('20m').ratings
 df, ue, ie = encode(df)
 m = to_csc(df)
 knn = get_knn(m, 10, 'cosine')
-sp = subprofiles(m, knn)
+sp = get_subprofiles(m, knn)
--- subprofiles.py
+++ subprofiles.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from multiprocessing import Pool, cpu_count
 
 
@@ -43,10 +44,58 @@ def merge_subprofiles(candidates, threshold):
     return candidates
 
 
-def subprofiles(ui_matrix, knn, threshold=0.5):
+def get_subprofiles(ui_matrix, knn, threshold=0.5):
     users = np.unique(ui_matrix.nonzero()[0])
     with Pool(cpu_count()) as p:
         res = p.starmap(
             get_user_subprofiles, [(ui_matrix, user, knn, threshold) for user in users],
         )
         return dict(zip(users, res))
+
+
+def rerank(items, scores, subprofiles, k, lmbd=0.5):
+    users = items.keys()
+    with Pool(cpu_count()) as p:
+        res = p.starmap(
+            user_ranking,
+            [(items[user], scores[user], subprofiles[user], k, lmbd) for user in users],
+        )
+    return dict(zip(users, res))
+
+
+def user_ranking(items, scores, subprofiles, k, lmbd):
+    subprofile_prob = np.array([len(sp) for sp in subprofiles])
+    subprofile_prob /= sum(subprofile_prob)
+
+    items = pd.Series(items)
+    item_prob_den = np.array([sum(items.isin(sp) * scores) for sp in subprofiles])
+    not_from_subprofiles = item_prob_den == 0
+    if not_from_subprofiles.any():
+        item_prob_den[not_from_subprofiles] = 1
+
+    res = []
+    penalty = [1] * len(subprofiles)
+    for i in range(k):
+        obj = [
+            (1 - lmbd) * score + lmbd * diversity(
+                item, penalty, subprofiles, subprofile_prob, score / item_prob_den
+            )
+            for item, score in zip(items, scores)
+        ]
+        pos = np.argmax(obj)
+        pick = items[pos]
+        res.append(pick)
+        for j, sp in enumerate(subprofiles):
+            if pick in sp:
+                penalty[j] *= 1 - scores[pos]*item_prob_den[j]
+        items.pop(pos)
+        scores.pop(pos)
+    return res
+
+
+def diversity(item, penalty, subprofiles, subprofile_prob, item_prob):
+    div = 0
+    for i, sp in enumerate(subprofiles):
+        if item in sp:
+            div += subprofile_prob[i] * item_prob[i] * penalty[i]
+    return div
